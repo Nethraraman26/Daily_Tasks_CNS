34195bbcdcf4101c639a704096ce1a81f63f151c
Nethraraman26 <nethraraman2006@gmail.com>
2026-03-18 22:08:07 +0530

    Enhance palindrome.java with detailed comments
    Added comments explaining the palindrome logic step-by-step.

--- day3/palindrome.java
+++ day3/palindrome.java
@@ -17,3 +17,38 @@ public class Main{
         }
     }
 }
+
+// n = 121
+// temp = 121
+// num = 0
+// 1:
+//     n = 121
+//     num = num * 10 + (n % 10)  -(takes the last digit) 
+//         = 0 * 10 + 1
+//         = 1
+//     n = n / 10  -(removes the last digit)
+//       = 121 / 10
+//       = 12
+
+// 2:
+//     num=1
+//     n=12
+//     num = num * 10 + (n % 10)
+//         = 1 * 10 + 2
+//         = 12
+//     n = n / 10
+//       = 12 / 10
+//       = 1
+
+// 3:
+//     num=12
+//     n=1
+//     num = num * 10 + (n % 10)
+//     = 12 * 10 + 1
+//     = 121
+//     n = n / 10
+//       = 1 / 10
+//       = 0
+
+// n=0(so the loop ends)
+    
